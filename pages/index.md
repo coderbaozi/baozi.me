b8ba38e2998432fb4d7ac92174e738e036f81abc
coderbaozi <YHbaozi1988@163.com>
2023-04-19 21:04:41 +0800

    :style: modify some style

--- pages/index.md
+++ pages/index.md
@@ -2,4 +2,4 @@
 title: Baozi
 ---
 
-Hi, I am baozi, a web application developmenter.
+Hi, I am **baozi**, a web application developmenter.Currently a sophomore majoring in CS.Like to explore front-end engineering solutions
--- pages/project/index.js
+++ pages/project/index.js
@@ -1,6 +1,6 @@
-import Layout from "../../components/layout"
-import Project from "../../components/Project"
-import style from '../../styles/listproject.module.css'
+import Layout from "../../components/layout";
+import Project from "../../components/Project";
+import style from "../../styles/listproject.module.css";
 export default function ProjectList() {
   return (
     <Layout>
@@ -8,22 +8,42 @@ export default function ProjectList() {
         <h1 className={style.headline}>Projects</h1>
         <i className={style.about}>A collection of individual works</i>
       </div>
-      
+
       <div className={style.mark}>Game</div>
       <div className={style.projectsarea}>
-        <Project repo='https://github.com/coderbaozi/react-tic-tac-toe' name='Tic-Tac-Toe' tech='React' icon='jingziqi' description='A sample game for Learn React'></Project>
+        <Project
+          repo="https://github.com/coderbaozi/react-tic-tac-toe"
+          name="Tic-Tac-Toe"
+          icon="jingziqi"
+          description="A sample game for Learn React"
+        ></Project>
       </div>
 
       <div className={style.mark}>Function</div>
       <div className={style.projectsarea}>
-        <Project repo='https://github.com/coderbaozi/baozi-Navigation' name='Navigation' tech='Vue' icon='search' description='A convenient tab start page'></Project>
-        <Project repo='https://github.com/coderbaozi/sampleSystem' name='sampleSystem' tech='Vue' icon='admin' description='TypeScript-based background management system'></Project>
+        <Project
+          repo="https://github.com/coderbaozi/baozi-Navigation"
+          name="Navigation"
+          icon="search"
+          description="A convenient tab start page"
+        ></Project>
+        <Project
+          repo="https://github.com/coderbaozi/sampleSystem"
+          name="sampleSystem"
+          icon="admin"
+          description="TypeScript-based background management system"
+        ></Project>
       </div>
 
       <div className={style.mark}>Learn</div>
       <div className={style.projectsarea}>
-        <Project repo='https://github.com/coderbaozi/vue-question' name='vue-question' tech='' icon='question' description='Project issue gathered some Front End konwledge'></Project>
+        <Project
+          repo="https://github.com/coderbaozi/vue-question"
+          name="vue-question"
+          icon="question"
+          description="Project issue gathered some Front End konwledge"
+        ></Project>
       </div>
     </Layout>
-  )
+  );
 }
